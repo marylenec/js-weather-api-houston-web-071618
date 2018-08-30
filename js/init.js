acfca11522f68ccc97ad678d40732c22ac211406
Marylene Chan <marylenechan@Marylenes-MacBook-Pro-2.local>
2018-08-30 15:37:47 -0500

    Done

--- js/init.js
+++ js/init.js
@@ -1,12 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var API_KEY = "Go get an API key";
-  var CORS_WRAPPER = "https://cors-anywhere.herokuapp.com/"
-  var URL = CORS_WRAPPER + "http://api.wunderground.com/api/" + API_KEY + "/hourly/q/NY/New_York.json";
-  var ctx = document.getElementById("NYCWeatherChart").getContext("2d")
+  const ctx = document.getElementById("NYCWeatherChart").getContext("2d")
+  const API_KEY = "308435004fc27a21a03ed45a253e2210";
+  const CORS_WRAPPER = "https://cors-anywhere.herokuapp.com/"
+  const URL = CORS_WRAPPER + "https://api.darksky.net/forecast/" + API_KEY + "/40.7127,-74.0059?exclude=currently,minutely,daily";
 
-
-  makeRequest(URL, function(json) {
-    var data = generateDataSet(getHours(json), getFahrenheits(json));
-    var tempChart = new Chart(ctx).Line(data, { bezierCurve: true });
-  });
+  // make your fetch in the makeRequest function in weatherChart.js
+  makeRequest(URL, ctx)
 });
--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,50 @@
-function getFahrenheits(result){
+function getFahrenheit(result){
   // Your code goes here
+  return result.map(function(hour) {
+    return hour.temperature
+  })
 }
 
-function getHours(result){
+function getHour(result){
   // Your code goes here
+  return result.map(function(hour) {
+    let myDate = new Date(hour.time * 1000)
+    return myDate.getHours()
+  })
 }
 
 function generateDataSet(labels, data) {
-  // Your code goes here
+  return {
+    type: 'line',
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: "NYC Weather Data",
+          data: data,
+          backgroundColor: "rgba(100,150,220,0.2)",
+          borderColor: 'rgb(255, 99, 132)'
+        }
+      ]
+    },
+    options: {
+      // additional configurations here
+    }
+  }
 }
 
-function makeRequest(endpoint, success) {
+
+
+function makeRequest(endpoint, canvas) {
   // Your code goes here
+  fetch(endpoint)
+    .then(response =>response.json())
+  // .then(json=>console.log(json));
+  .then (function(json) //console.log(json.hourly.data)
+    {
+      let hourlyData = json.hourly.data
+      // After your fetch works - use your json data with the line below :)
+      const tempChart = new Chart(canvas, generateDataSet(getHour(hourlyData), getFahrenheit(hourlyData)))
+    }
+  );
 }
